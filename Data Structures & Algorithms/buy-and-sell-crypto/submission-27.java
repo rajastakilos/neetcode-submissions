0c6197bdd6826a764c594a7b753ec6a66295d8bc
Rajas D Pargaonkar <rajastakilos@users.noreply.github.com>
2026-09-10 18:26:39 -0400

    Add: buy-and-sell-crypto - submission-27

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-27.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int lowestDay = prices[0];
+
+        for (int i = 1; i < prices.length; i++) {
+            maxProfit = Math.max(maxProfit, prices[i] - lowestDay);
+            lowestDay = Math.min(lowestDay, prices[i]);
+        }
+
+        return maxProfit;
+    }
+}
